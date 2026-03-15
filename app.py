d049b34b47b0f0c614c04f85a45d0b797acb9940
Skollvr <thiago.camporezi@hotmail.com>
2026-03-15 14:49:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3092,7 +3092,7 @@ render_summary_cards(
         min_minutes=min_minutes,
         role_filter=role_filter,
     )
-    render_game_rankings(
+render_game_rankings(
         away_df=away_df,
         home_df=home_df,
         min_games=min_games,
